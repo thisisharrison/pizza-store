15f14dee810ec1043bb3319d310b761a9612ba24
Harrison Lau <harrison.lau@outlook.com>
2021-12-04 18:10:31 +0800

    fix: eslint error

--- src/__tests__/PizzaStore.test.tsx
+++ src/__tests__/PizzaStore.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "../setupTests";
-import { screen, waitFor, within } from "@testing-library/react";
+import { screen, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Home from "../module/Home";
 
